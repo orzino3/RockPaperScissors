d6326ca4b557b071b38a0669290932e7dfe6c694
orzino3 <155395782+orzino3@users.noreply.github.com>
2024-10-15 18:34:41 +0300

    Update rockpaper.js

--- rockpaper.js
+++ rockpaper.js
@@ -2,60 +2,62 @@
           let compChoice;
           let result;
           
-          let score = {
+          let score = JSON.parse(localStorage.getItem('score')) || {
             wins: 0,
             loses: 0,
             tie: 0
           };
-          
+
+          updateScore();
+         
 
           function rockCondition(){
             if(compChoice === userChoice){
-                result = 'tie'
+                result = "It's a tie!"
                 score.tie++;
               }
 
               else if (compChoice === 'paper'){
-                result = 'You lose';
+                result = 'You lose!';
                 score.loses++;
               }
 
               else if (compChoice === 'scissors'){
-                result = 'You win';
+                result = 'You win!';
                 score.wins++;
               }
           }
 
           function paperCondition(){
             if(compChoice === userChoice){
-                  result = 'tie'
+                  result = "It's a tie!"
                   score.tie++;
                 }
 
                 else if (compChoice === 'rock'){
-                  result = 'You win';
+                  result = 'You win!';
                   score.wins++;
                 }
 
                 else if (compChoice === 'scissors'){
-                  result = 'You lose';
+                  result = 'You lose!';
                   score.loses++;
                 }
           }
 
           function scissorsCondition(){
             if(compChoice === userChoice){
-                  result = 'tie'
+                  result = "It's a tie!"
                   score.tie++;
                 }
 
                 else if (compChoice === 'paper'){
-                  result = 'You win';
+                  result = 'You win!';
                   score.wins++;
                 }
 
                 else if (compChoice === 'rock'){
-                  result = 'You lose';
+                  result = 'You lose!';
                   score.loses++;
                 }
           }
@@ -72,10 +74,31 @@
               else if(playerMove === 'scissors'){
               scissorsCondition();
               }
+              
+              localStorage.setItem('score', JSON.stringify(score));
 
+              updateScore();
+              
+              document.querySelector('.js-result').innerHTML = result;
 
-            alert(`You picked ${userChoice}. Computer picked ${compChoice}. ${result}
-Wins: ${score.wins}. Loses: ${score.loses}. Ties: ${score.tie}`);
+              document.querySelector('.js-moves').innerHTML = `Player: ${playerMove}
+              || Computer: ${compChoice}`;
+
+            /*alert(`You picked ${userChoice}. Computer picked ${compChoice}. ${result}
+Wins: ${score.wins}. Loses: ${score.loses}. Ties: ${score.tie}`);*/
+          }
+
+          function updateScore(){
+            document.querySelector('.js-score').innerHTML = `
+Wins: ${score.wins}. Loses: ${score.loses}. Ties: ${score.tie}`;
+          }
+
+          function updateResult(){
+            document.querySelector('.js-result').innerHTML = result;
+          }
+
+          function updateMoves(){
+            document.querySelector('.js-moves').innerHTML = `You: ${playerMove} - Computer: ${compChoice}`;
           }
 
           function generateCompMove(){
@@ -94,8 +117,16 @@ Wins: ${score.wins}. Loses: ${score.loses}. Ties: ${score.tie}`);
           }
           }
 
+          function cleanResult(){
+            document.querySelector('.js-result').innerHTML = '';
+            document.querySelector('.js-moves').innerHTML = '';
+          }
+
           function resetScore(){
             score.loses = 0;
             score.wins = 0;
             score.tie = 0;
+            updateScore();
+            cleanResult();
+            
           }
